a25d6fc7c48cd551ec1efce081521a5d8693ffd4
Forest Pearson <flpearson.fp@gmail.com>
2022-05-09 15:53:50 -0700

    Function to sync the DB with the MTGSDK api.

--- README.md
+++ README.md
@@ -76,9 +76,9 @@ In order to ensure that the Laravel community is welcoming to all, please review
 If you discover a security vulnerability within Laravel, please send an e-mail to Taylor Otwell via [taylor@laravel.com](mailto:taylor@laravel.com). All security vulnerabilities will be promptly addressed.
 
 ## Team members
-- Forest Pearson
-- Ian Wasson
-- Zachary Chronister
+- Forest Pearson, API dev
+- Ian Wasson, Data management
+- Zachary Chronister, UX design
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,4 +16,33 @@ class AdminController extends Controller {
     public function show() {
         return view('admin');
     }
+
+    //Syncs the DB and should only need to be run when either
+    //the DB is empty or the MTGSDK is updated.
+    public function syncDB(){
+        //Copy all cards over from the MTG SDK.
+        $cards = Card::all();
+
+        //loop through all cards and insert them into the DB.
+        foreach($cards as $card){
+            //Check if the card is already in the DB.
+            $card_current = Cards::where('card_id', $card->id)->first();
+            //If it is, update the card.
+            if(!$card_current){
+                $card_model = new Cards;
+                $card_model->card_id = $card->id;
+                $card_model->card_name = $card->name;
+                $card_model->manacost = $card->manaCost;
+                $card_model->colors = $card->colors;
+                $card_model->type = $card->type;
+                $card_model->rarity = $card->rarity;
+                $card_model->card_set = $card->set;
+                $card_model->multiverseid = $card->multiverseId;
+                $card_model->image_url = $card->imageUrl;
+                $card_model->save();
+            }
+        }
+        //return the view with the cards.
+        return redirect()->route('admin')->with(['success' => 'Cards synced successfully.']);
+    }
 }
