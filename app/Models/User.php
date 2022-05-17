bc5d2ea23938abef2623451e3d1bca3232bd90dc
Forest Pearson <flpearson.fp@gmail.com>
2022-05-17 16:09:50 -0700

    Fixed DB user access
    Had to mess around with PHP migration and other DB services.

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ class User extends Authenticatable
 {
   use HasApiTokens, Notifiable;
   protected $fillable = [
-      'name', 'email', 'password',
+      'first_name', 'last_name', 'email', 'password',
   ];
   protected $hidden = [
        'password', 'remember_token',
