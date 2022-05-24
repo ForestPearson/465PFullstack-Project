4167b2c39f0be3081412f1b26fa072d4a2fe606b
Bishop <iwasson_cat@cat.pdx.edu>
2022-05-24 15:36:55 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\CardController;
 
 /*
 |--------------------------------------------------------------------------
