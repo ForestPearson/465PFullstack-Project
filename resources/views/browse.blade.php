45e3175c586bf70b0e16588ed41344de1793fde5
zchronis <60366837+zchronis@users.noreply.github.com>
2022-05-24 16:26:07 -0700

    formmating browse page

--- resources/views/browse.blade.php
+++ resources/views/browse.blade.php
@@ -5,14 +5,29 @@
 
 <div class="container pt-4 bootstrap-grid text-light mb-5">
     @foreach ($allCards as $card)
-    <img
-        class="align-middle"
-        src="{{ $card->image_url; }}"
-        alt="Logo Icon" 
-    />
+        <img
+            class="align-middle m-2 mtgCard hover"
+            src="{{ $card->image_url; }}"
+            alt="Logo Icon" 
+            height="300em"
+        />
     @endforeach
 </div>
 
 @endsection
 
-<!--$allCards[0]->card_name-->
+<!--$allCards[0]->card_name
+tags:
+
+id
+card_name
+manacost
+colors
+type
+rarity
+card_set
+multiverseid
+image_url
+created_at
+updated_at
+-->
