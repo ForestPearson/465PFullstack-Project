e4fbc2464810533ac2fb1015c9b13c283db8d3fb
zchronis <60366837+zchronis@users.noreply.github.com>
2022-05-24 15:53:34 -0700

    started browse frontend

--- resources/views/browse.blade.php
+++ resources/views/browse.blade.php
@@ -4,7 +4,15 @@
 @section('body')
 
 <div class="container pt-4 bootstrap-grid text-light mb-5">
-    
+    @foreach ($allCards as $card)
+    <img
+        class="align-middle"
+        src="{{ $card->image_url; }}"
+        alt="Logo Icon" 
+    />
+    @endforeach
 </div>
 
 @endsection
+
+<!--$allCards[0]->card_name-->
