ecec94a0bfad263f199ee70839a507186c040dbb
Bishop <iwasson_cat@cat.pdx.edu>
2022-05-24 16:00:25 -0700

    Fixed cards

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -13,7 +13,7 @@ use App\Models\Accounts;
 class CardController extends Controller {
 
     public function show() {
-        $allCards = Cards::orderBy('card_name')->paginate(50);
+        $allCards = Cards::distinct('card_name')->orderBy('card_name')->paginate(50);
 
         return view('browse', compact('allCards'));
     }
--- routes/api.php
+++ routes/api.php
@@ -29,10 +29,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->group(function () {
-  Route::get('getCardByName', [AuthController::class,'getCardByName'])->name('getCardByName');
-  Route::get('getCardsByColor', [AuthController::class,'getCardsByColor'])->name('getCardsByColor');
-  Route::get('getCardsBySet', [AuthController::class,'getCardsBySet'])->name('getCardsBySet');
-  Route::get('getCardsByType', [AuthController::class,'getCardsByType'])->name('getCardsByType');
-  Route::get('getCardsByRarity', [AuthController::class,'getCardsByRarity'])->name('getCardsByRarity');
+Route::controller(CardController::class)->group(function() {
+  Route::get('getCardByName', [CardController::class,'getCardByName'])->name('getCardByName');
+  Route::get('getCardsByColor', [CardController::class,'getCardsByColor'])->name('getCardsByColor');
+  Route::get('getCardsBySet', [CardController::class,'getCardsBySet'])->name('getCardsBySet');
+  Route::get('getCardsByType', [CardController::class,'getCardsByType'])->name('getCardsByType');
+  Route::get('getCardsByRarity', [CardController::class,'getCardsByRarity'])->name('getCardsByRarity');
 });
