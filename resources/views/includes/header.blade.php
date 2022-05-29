eb1e56c84efbc3e679cf7e8cda638f41fe8e0723
Bishop <iwasson_cat@cat.pdx.edu>
2022-05-29 15:24:27 -0700

    Update header.blade.php
    FAVICON!

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,8 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
+        
 
         <title>MTG Deck Builder</title>
+        <link rel="icon" type="image/x-icon" href="{{ asset('image/logo4.png') }}">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
