627076cf6d4ea42febada713fac1c3cebb10de75
zchronis <60366837+zchronis@users.noreply.github.com>
2022-05-29 13:53:14 -0700

    formatted admin page

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -2,30 +2,36 @@
 
 @section('body')
 
-<div class="container d-flex justify-content-center row">
-    <div class="col-8">
-        @if(count($allUsers))
-            @foreach($allUsers as $account)
-                @if($account->id != $user->id)
-                <div class="card">
-                    <div class="card-body">
-                        <p class="card-text">{{ $account->first_name }}</p>
-                        <p class="card-text">{{ $account->last_name }}</p>
-                        <p class="card-text">{{ $account->email }}</p>
-                        @if($account->is_admin)
-                            <a href="{{ route('revokeAdmin', ['id' => $account->id]) }}" class="btn btn-success">Revoke Admin</a>
-                        @else
-                            <a href="{{ route('makeAdmin', ['id' => $account->id]) }}" class="btn btn-success">Make Admin</a>
-                        @endif
-                        <a href="{{ route('deleteAccount', ['id' => $account->id]) }}" class="btn btn-danger">Delete</a>
+<div class="container">
+    <div class="row">
+        <div class="col-8 d-flex flex-wrap justify-content-center">
+            @if(count($allUsers))
+                @foreach($allUsers as $account)
+                    @if($account->id != $user->id)
+                    <div class="card m-2 accountCard border border-white rounded border-2">
+                        <div class="card-body">
+                            <p class="card-text">Name: {{ $account->first_name }} {{ $account->last_name }}</p>
+                            <hr>
+                            <p class="card-text">Email: {{ $account->email }}</p>
+                            <hr>
+                            @if($account->is_admin)
+                                <a href="{{ route('revokeAdmin', ['id' => $account->id]) }}" class="btn btn-warning">Revoke Admin</a>
+                            @else
+                                <a href="{{ route('makeAdmin', ['id' => $account->id]) }}" class="btn btn-success">Make Admin</a>
+                            @endif
+                            <a href="{{ route('deleteAccount', ['id' => $account->id]) }}" class="btn btn-danger float-end"><i class="fa-solid fa-trash-can"></i></a>
+                        </div>
                     </div>
-                @endif
-            @endforeach
-        @endif
-    </div>
-
-    <div class="col-4">
-        <button onclick="window.location='{{ route("sync") }}'" class="btn btn-primary col mx-1">Sync Database</button>
+                    @endif
+                @endforeach
+            @endif
+        </div>
+        
+        <div class="col-2 py-2 ">
+            <div class="border border-warning rounded border-2 d-flex justify-content-center p-2">
+                <button onclick="window.location='{{ route("sync") }}'" class="btn btn-primary col mx-1">Sync Database</button>
+            </div>
+        </div>
     </div>
 </div>
 
