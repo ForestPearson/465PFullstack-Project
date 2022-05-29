af74384871705fe14c63c42bc33a6c6ef8dbbec5
Bishop <iwasson_cat@cat.pdx.edu>
2022-05-28 18:27:39 -0700

    Update modify.blade.php

--- resources/views/modify.blade.php
+++ resources/views/modify.blade.php
@@ -22,23 +22,24 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="modifyCardModal" tabindex="-1" aria-labelledby="modifyCardModal" aria-hidden="true">
-  <div class="modal-dialog modal-sm">
-    <div class="modal-content customModalStyle">
-      <div class="modal-header border-warning">
-        <h4 class="modal-title text-center" id="modifyCardModalLabel">Modify Card</h4>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body d-flex justify-content-center">
-        ...
-      </div>
-      <div class="modal-footer d-flex justify-content-center container border-warning">
-        <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
-      </div>
+<form>
+    <div class="modal fade" id="modifyCardModal" tabindex="-1" aria-labelledby="modifyCardModal" aria-hidden="true">
+        <div class="modal-dialog modal-sm">
+            <div class="modal-content customModalStyle">
+            <div class="modal-header border-warning">
+                <h4 class="modal-title text-center" id="modifyCardModalLabel">Modify Card</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body d-flex justify-content-center">
+                ...
+            </div>
+            <div class="modal-footer d-flex justify-content-center container border-warning">
+                <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
+</form>
 
 
 <script>
