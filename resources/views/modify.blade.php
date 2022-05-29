8fc89c6bebb893a971e0c74191c4ab13fbcea4dc
zchronis <60366837+zchronis@users.noreply.github.com>
2022-05-28 18:09:51 -0700

    Update modify.blade.php

--- resources/views/modify.blade.php
+++ resources/views/modify.blade.php
@@ -1,12 +1,12 @@
 @extends('includes.header')
 @section('body')
 
-<div class="container text-light text-center">
+<div class="container text-light text-center pt-5">
     <h1>{{$deck->name}}</h1>
 </div>
 
 @if(count($deck->cards))
-<div class="container mb-5 py-5 d-flex justify-content-center flex-wrap">
+<div class="container mb-5 py-2 d-flex justify-content-center flex-wrap">
     @foreach($deck->cards as $cardRel)
         <img 
             src="{{$cardRel->card->image_url}}" 
@@ -23,18 +23,18 @@
 
 <!-- Modal -->
 <div class="modal fade" id="modifyCardModal" tabindex="-1" aria-labelledby="modifyCardModal" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-sm">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="modifyCardModalLabel">Modify Card</h5>
+        <h5 class="modal-title text-center" id="modifyCardModalLabel">Modify Card</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
+      <div class="modal-body d-flex justify-content-center">
         ...
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+      <div class="modal-footer d-flex justify-content-center container">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-danger">Remove from Deck</button>
       </div>
     </div>
   </div>
@@ -43,7 +43,7 @@
 
 <script>
     function populateModal(imageUrl) {
-        $('#modifyCardModal .modal-body').html('<img src="' + imageUrl + '" style="height: 300px">');
+        $('#modifyCardModal .modal-body').html('<img src="' + imageUrl + '" style="height: 400px">');
     }
 
 </script>
